e4401407b3cf279413511bf7bd1827c6d6607034
Harley Alexander <h@pachama.com>
2023-07-12 15:21:07 +1000

    Initial commit

---
+++ apps/panda-nx-next-test/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'panda-nx-next-test',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/panda-nx-next-test',
+};
